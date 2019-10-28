904b4067701fd70151516480262d40da6b09eba6
LeannaMulv <40302225+LeannaMulv@users.noreply.github.com>
2019-10-28 11:02:59 -0400

    Update pictureServer.js

--- pictureServer.js
+++ pictureServer.js
@@ -179,19 +179,19 @@ void loop() {
     /// First, we create a name for the new picture.
     /// The .replace() function removes all special characters from the date.
     /// This way we can use it as the filename.
-    var imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
+      var imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
 
-    console.log('making a making a picture at'+ imageName); // Second, the name is logged to the console.
+      console.log('making a making a picture at'+ imageName); // Second, the name is logged to the console.
 
-    //Third, the picture is  taken and saved to the `public/`` folder
-    NodeWebcam.capture('public/'+imageName, opts, function( err, data ) {
-    io.emit('newPicture',(imageName+'.jpg')); ///Lastly, the new name is send to the client web browser.
-    /// The browser will take this new name and load the picture from the public folder.
+      //Third, the picture is  taken and saved to the `public/`` folder
+      NodeWebcam.capture('public/'+imageName, opts, function( err, data ) {
+      io.emit('newPicture',(imageName+'.jpg')); ///Lastly, the new name is send to the client web browser.
+      /// The browser will take this new name and load the picture from the public folder.
   });
 
-  //});
+  });
   // END LEANNA ADDITION
-    }
+  }
   else{
       Serial.println("dark");
     }

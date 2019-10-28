23f28ad910de1f290666e16ec5a93f01f0e09e01
LeannaMulv <40302225+LeannaMulv@users.noreply.github.com>
2019-10-28 19:09:05 -0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -43,19 +43,19 @@ socket.on('server-msg', function(msg) {
     case "light":
       document.body.style.backgroundColor = "white";
       console.log("white")
-      //('takePicture', function() {
-      /// First, we create a name for the new picture.
-      /// The .replace() function removes all special characters from the date.
-      /// This way we can use it as the filename.
-      var imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
+//       //('takePicture', function() {
+//       /// First, we create a name for the new picture.
+//       /// The .replace() function removes all special characters from the date.
+//       /// This way we can use it as the filename.
+//       var imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
 
-      console.log('making a making a picture at'+ imageName); // Second, the name is logged to the console.
+//       console.log('making a making a picture at'+ imageName); // Second, the name is logged to the console.
 
-      //Third, the picture is  taken and saved to the `public/`` folder
-      NodeWebcam.capture('public/'+imageName, opts, function( err, data ) {
-      io.emit('newPicture',(imageName+'.jpg')); ///Lastly, the new name is send to the client web browser.
-      /// The browser will take this new name and load the picture from the public folder.
-    //});
+//       //Third, the picture is  taken and saved to the `public/`` folder
+//       NodeWebcam.capture('public/'+imageName, opts, function( err, data ) {
+//       io.emit('newPicture',(imageName+'.jpg')); ///Lastly, the new name is send to the client web browser.
+//       /// The browser will take this new name and load the picture from the public folder.
+//     //});
       break;
     case "dark":
       document.body.style.backgroundColor = "black";
